aa81dd219fcb0efeb96cfbeace0b34980385351d
dB-DigitalFox-DanielBrilla <91141570+dB-Digital-Fox@users.noreply.github.com>
2022-12-15 12:33:35 +0100

    Add files via upload
    Matrix - generates random numbers
    troll opens rick roll and hafo song and locks user

---
+++ troll.bat
@@ -0,0 +1,8 @@
+: x
+nircmd.exe mutesysvolume 0
+nircmd.exe setsysvolume 65535
+start https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start https://www.youtube.com/watch?v=IX3j84DJAGo
+rundll32.exe user32.dll, LockWorkStation
+timeout /t 15
+goto x
